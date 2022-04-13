b4bc8f7e1f5d604b1de57dae99d10ab034568023
GreedNeSS <ruslan199126@gmail.com>
2022-04-13 21:16:28 +0300

    Добавьте файлы проекта.

---
+++ TCP_Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("***** TCP Client *****");
+
+int port = 8888;
+string address = "127.0.0.1";
+
+using (TcpClient client = new TcpClient())
+{
+    try
+    {
+        client.Connect(address, port);
+        byte[] buffer = new byte[256];
+        StringBuilder response = new StringBuilder();
+
+        using (NetworkStream stream = client.GetStream())
+        {
+            do
+            {
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                response.Append(Encoding.UTF8.GetString(buffer));
+            }
+            while (stream.DataAvailable);
+
+            Console.WriteLine(response);
+        }
+    }
+    catch (SocketException ex)
+    {
+        Console.WriteLine("SocketException: " + ex.Message);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Exception: " + ex.Message);
+    }
+
+    Console.WriteLine("Запрос завершён!");
+    Console.ReadLine();
+}
